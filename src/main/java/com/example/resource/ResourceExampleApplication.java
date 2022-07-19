c8afd8b3ded742e5dc2dec584fcce94ae63e54fb
Anton <antonsashkov@gmail.com>
2022-07-19 23:11:49 +0200

    renaming

--- src/main/java/com/example/resource/ResourceExampleApplication.java
+++ src/main/java/com/example/resource/ResourceExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvc2Application {
+public class ResourceExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMvc2Application.class, args);
+        SpringApplication.run(ResourceExampleApplication.class, args);
     }
 
 }
--- src/test/java/com/example/resource/ResourceExampleApplicationTests.java
+++ src/test/java/com/example/resource/ResourceExampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringMvc2ApplicationTests {
+class ResourceExampleApplicationTests {
 
     @Test
     void contextLoads() {
