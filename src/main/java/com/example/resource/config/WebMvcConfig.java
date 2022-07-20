ae86fbe3b8ca248a45f062807738938edc534812
Anton <antonsashkov@gmail.com>
2022-07-20 16:55:47 +0200

    add resource handlers

--- src/main/java/com/example/resource/config/WebMvcConfig.java
+++ src/main/java/com/example/resource/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import com.example.resource.interceptor.LogInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(logInterceptor);
         registry.addInterceptor(authorizationInterceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/json/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/txt/");
     }
 }
